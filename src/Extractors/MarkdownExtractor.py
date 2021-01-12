7b0f1c7ead1c8141d2557b6a87d456650f3a7c36
Azmain Amin <mdazmainamin@gmail.com>
2021-01-12 11:27:16 -0500

    add docstring

--- src/Extractors/MarkdownExtractor.py
+++ src/Extractors/MarkdownExtractor.py
@@ -13,6 +13,10 @@ class MarkdownNoteExtractor(NoteExtractorInterface):
         self.noteBody = markdown.markdown(text)
 
     def getRandomlyChosenNotes(self, numToReturn):
+        """
+        Returns a randomly selected number (based on the numToReturn arg) of 
+        notes/highlights from the collection of notes (self.noteBody)
+        """
         allNoteText = self._getAllNoteTexts()
         if len(allNoteText) < numToReturn:
             return allNoteText
@@ -25,6 +29,10 @@ class MarkdownNoteExtractor(NoteExtractorInterface):
             return randomlyChosenNotes
 
     def _getAllNoteTexts(self):
+        """
+        A single piece of note/highlight is denoted as such: <h2>Note 1 Title</h2> NOTE_TO_BE_EMAILED <h2> Note 2 Title</h2>.
+        Basically, treat everything between two h2 headers as pieces of notes.
+        """
         soup = BeautifulSoup(self.noteBody, "html.parser")
         h2_tags = soup.find_all("h2")
         all_notes = []
@@ -33,17 +41,18 @@ class MarkdownNoteExtractor(NoteExtractorInterface):
             next_node = header
             while True:
                 next_node = next_node.nextSibling
-
                 if next_node is None:
                     break
                 if isinstance(next_node, Tag):
                     name = next_node.name 
                     if name == 'h2':
                         break
+                    # Support code blocks
                     if "```" in next_node.contents[0]:
                         code_tag = BeautifulSoup().new_tag('code')
                         code_tag.append(next_node.contents[0])
                         results.append(code_tag)
+                    #TODO: Support images
                     if name == 'p' or name == 'ul':
                         results.append(next_node)
             reduced = self._appendTags(results)
